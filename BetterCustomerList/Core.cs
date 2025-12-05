5b27d44f5d971fbd08dac12beb56524001bf09a4
PyrexBLJ <49227284+PyrexBLJ@users.noreply.github.com>
2025-12-05 09:49:03 -0500

    better customer list 1.0.1

--- BetterCustomerList/Core.cs
+++ BetterCustomerList/Core.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 using HarmonyLib;
 using System.Reflection;
 using System;
+using System.Drawing;
 
-[assembly: MelonInfo(typeof(BetterCustomerList.Core), "BetterCustomerList", "1.0.0", "Pyrex", null)]
+[assembly: MelonInfo(typeof(BetterCustomerList.Core), "BetterCustomerList", "1.0.1", "Pyrex", null)]
 [assembly: MelonGame("TVGS", "Schedule I")]
 
 namespace BetterCustomerList
@@ -15,20 +16,34 @@ namespace BetterCustomerList
     public class Core : MelonMod
     {
         private static HarmonyLib.Harmony harmony;
+        private MelonPreferences_Category category;
+        private static MelonPreferences_Entry<float> fontsize;
+        private static MelonPreferences_Entry<bool> showregion;
         public override void OnInitializeMelon()
         {
             LoggerInstance.Msg("Better Customer List Loaded.");
+            category = MelonPreferences.CreateCategory("BetterCustomerList", "Better Customer List");
+            fontsize = category.CreateEntry<float>("TextSize", 22.0f, "Text size", "Set the size of the font used to make each customer entry, to fit more info on screen. changes show after save quit.");
+            showregion = category.CreateEntry<bool>("ShowRegion", true, "Show Region", "Add the region the customer is from at the end of their listing. changes show after save quit.");
         }
         private static bool CreateEntry_Hook(Customer customer, CustomerSelector __instance)
         {
             RectTransform component = UnityEngine.Object.Instantiate<GameObject>(__instance.ButtonPrefab, __instance.EntriesContainer).GetComponent<RectTransform>();
             component.Find("Mugshot").GetComponent<UnityEngine.UI.Image>().sprite = customer.NPC.MugshotSprite;
             float relation_percent = (customer.NPC.RelationData.RelationDelta / 5) * 100;
-            string full_string;
+            string full_string = $"<size={fontsize.Value}>{customer.NPC.fullName}    ";
             if (relation_percent == 100.0f)
-                full_string = $"{customer.NPC.fullName}    <color=#ffcc00>{relation_percent}%</color>";
+                full_string += $"<color=#ffcc00>{relation_percent}%</color>";
             else
-                full_string = $"{customer.NPC.fullName}    {relation_percent}%";
+                full_string += $"{relation_percent}%";
+            if (showregion.Value == true)
+            {
+                full_string += $" ({customer.NPC.Region})</size>";
+            }
+            else
+            {
+                full_string += "</size>";
+            }
             component.Find("Name").GetComponent<UnityEngine.UI.Text>().text = full_string;
             component.GetComponent<Button>().onClick.AddListener((UnityEngine.Events.UnityAction)(() => __instance.CustomerSelected(customer)));
             __instance.customerEntries.Add(component);
